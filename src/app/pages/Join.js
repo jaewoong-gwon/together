f626dd5b25d23c9c3366f018c06098f24e5a07cd
jaewoong-gwon <wodndrnjs@naver.com>
2022-08-30 00:03:19 +0900

    게시판 구현(3)

--- src/app/pages/Join.js
+++ src/app/pages/Join.js
@@ -1,12 +1,45 @@
 import React from "react";
-import { Paper, Container, Button,Box } from "@mui/material/";
-import {Table,TableBody,TableCell,TableContainer,TableHead,TableRow } from "@mui/material/";
+import { Paper, Container, Button,Box,Checkbox } from "@mui/material/";
+import {Table,TableBody,TableCell,TableContainer,TableHead,TableRow,TablePagination } from "@mui/material/";
 import {useState,useEffect} from "react";
 import axios from "axios";
 
 function Join() {
 
     const [rows,setRows] = useState([]);
+    const [checkedBox, setCheckedBox] = useState([]); //check 된 checkBox 를 관리. id값 즉 db에 바로 사용 가능한 key 값을 저장.
+    const [checked,setChecked] = useState(false);
+    const [page,setPage] = useState(0);
+    const [rowsPerPage, setRowsPerPage] = React.useState(10);
+
+    const handleChildChange = (event) => {
+        console.log(event.target);
+        const key = event.target.id; // String
+        if(event.target.checked){ setCheckedBox([...checkedBox, {id : key}]);}
+
+        else {
+            const temp = [...checkedBox];
+            const result = temp.filter(temp => temp.id != key);
+            //해당 조건리 t`rue면 요소를 유지, false면 버림) 새로운 배열로 반환
+            setCheckedBox(result);
+            console.log(checkedBox);
+            }
+        }
+
+    const handleParentChecked = (event) => {
+        console.log(event.target.className);
+        setChecked(event.target.checked);
+        if(checked){console.log(checkedBox);}
+    }
+
+    const handleChangePage = (event, newPage) => {
+        setPage(newPage);
+      };
+
+    const handleChangeRowsPerPage = (event) => {
+        setRowsPerPage(parseInt(event.target.value, 10));
+        setPage(0);
+      };
 
     useEffect(()=> {
         axios
@@ -24,6 +57,7 @@ return(
       <Table sx={{ minWidth: 650 }}>
         <TableHead>
           <TableRow>
+            <TableCell><Checkbox className="root" checked={checked} onChange={handleParentChecked}/></TableCell>
             <TableCell>번호</TableCell>
             <TableCell>제목</TableCell>
             <TableCell>내용</TableCell>
@@ -35,6 +69,8 @@ return(
             <TableRow
               key={row.boardId}
             >
+                {/*mui Checkbox 의 id prop은 Stirng type. row.boardId는 int */}
+              <TableCell><Checkbox id={row.boardId.toString()} onChange={handleChildChange}/></TableCell>
               <TableCell component="th" scope="row">
                 {row.boardId}
               </TableCell>
@@ -45,8 +81,17 @@ return(
           ))}
         </TableBody>
       </Table>
+          <TablePagination
+            component="div"
+            count={rows.length}
+            onPageChange={handleChangePage}
+            onRowsPerPageChange={handleChangeRowsPerPage}
+            page={page} rowsPerPage={10}
+            rowsPerPageOptions={[10]}/>
     </TableContainer>
+
     <Button href="/Write">글쓰기</Button>
+    <Button>삭제</Button>
 </Container>
 );
 }
